799df18b960389925285b99da8a899ca0f0b366e
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-09-12 21:15:58 +0530

    Create React1.js

---
+++ React1.js
@@ -0,0 +1,30 @@
+What is react?
+
+It is an open source librarary from building user interfaces
+Not a framework
+Focus on Ui
+Rich ecosystem 
+
+
+Why learn react ?
+
+Answer -
+Created and maintained facebook 
+More  than 100 k starts on github .
+Huge coommunity 
+In demand skillset.
+
+React is declarative 
+
+
+Prequistes
+
+Html, css and  javascript fundamentals
+Es6 
+Javascript- "This keyword,filter map and reduce"
+Es6 - let & const arrow functions,template literals ,default parameters ,object literals, rest and spread operators and destructing assignments
+React from scratch 
+
+Channel content=-React 
+
+Fundamentals Http Routing Redux utilities
