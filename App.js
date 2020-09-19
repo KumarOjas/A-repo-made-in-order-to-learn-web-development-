91c68ddf5de24083b612552a915c9ec96ce29cc1
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-09-19 13:27:08 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,82 @@
+/*
+
+Import React , {Component } from "React";
+class App extends Component  {
+  
+   render () {
+   return (
+     <div classname ="App">
+     <h1> My first React App </h1>
+     <p> Welcome :)  </p>
+     </div>
+     );
+     }
+     }
+     export default App ;
+     
+*/
+/*
+Import React , {Component } from "React";
+class App extends Component  {
+  
+   render () {
+   return (
+     <div classname ="App">
+     <h1> My first React App </h1>
+     <p> Welcome :)  </p>
+     </div>
+     );
+     }
+     }
+     export default App ;
+     
+  */
+  
+  import React, {Component} from "react";
+  import Ninjas from "./Ninjas";
+  
+  claas App extends Component {
+   render () {
+   return  (
+     <div classNmae="App">
+     <h1>My first React app! </h1>
+     <p> Welcome :) </p>
+     <Ninjas/>
+     </div>
+     );
+     }
+     }
+     export default Ninjas
+     
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+     
+     
+     
+     
+     
